aa292f5c764b221d692d8e95348c5cfbd064c935
IlhamMisbakhul <ilhammisbakhul123@gmail.com>
2024-06-21 21:22:56 +0700

    Menambahkan Halaman Index History Transaction

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:jb_store/models/product.dart';
+import 'package:jb_store/screen/all_product.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Product> cart = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Store App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: AllProductsScreen(), //Menghubungkan Ke all_product.dart sekaligus mendapatkan akses ke cart.dart
+      routes: {
+        '/all-products': (context) => AllProductsScreen(),
+      },
+    );
+  }
+}
